f7ac4826f15d94a7e9cb663a54a6e9dced0fd0a5
Dylan Ferrara <dylan.ferrara@gmail.com>
2018-01-11 04:59:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,3 +34,14 @@ function removeElementFromBeginningOfArray(array) {
   return array;
 }
 
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1);
+  return array;
+}
+
+function 
