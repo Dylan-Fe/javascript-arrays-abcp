395508b120a88494b506b1613b6e724f4a59b660
Dylan Ferrara <dylan.ferrara@gmail.com>
2018-01-11 04:46:13 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, e) {
+  array = [e, ...array];
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, e) {
+  
+}
